20a0cce1c5c5139ad13d10d8ce44cc95775c09db
kevinofficer <57766007+kevinofficer@users.noreply.github.com>
2025-11-11 16:20:31 +0000

    prepend govuk-link style

--- NIHR.GovUk.AspNetCore.Mvc/TagHelpers/LinkTagHelper.cs
+++ NIHR.GovUk.AspNetCore.Mvc/TagHelpers/LinkTagHelper.cs
@@ -7,14 +7,8 @@ namespace NIHR.GovUk.AspNetCore.Mvc.TagHelpers
     public class LinkTagHelper : TagHelper
     {
 
-        [HtmlAttributeName("no-govuk-link")]
-        public bool NoGovUkLink { get; set; }
-
         public override void Process(TagHelperContext context, TagHelperOutput output)
         {
-            return; // TODO: Review all links without govuk-link class. Class may change styles unexpectedly. Consider adding no-govuk-link.
-            if (NoGovUkLink)
-                return;
 
             output.PrependClass("govuk-link");
         }

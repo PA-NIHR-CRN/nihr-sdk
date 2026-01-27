0344e42419039b6763ef9e241734261fbe64b004
kira.clarke <kira.clarke@nihr.ac.uk>
2026-01-27 07:47:21 +0000

    CRNCC-1621 Make Created/Modifed properties nullable to be consistent with CPMS-DB

--- NIHR.CRN.CPMS.Abstractions/IRefPerson.cs
+++ NIHR.CRN.CPMS.Abstractions/IRefPerson.cs
@@ -9,12 +9,12 @@ where TAcl : IAcl
     public string? OrcId { get; set; }
     bool Active { get; set; }
     
-    public DateTime CreatedDate { get; set; }
-    public DateTime ModifiedDate { get; set; }
+    public DateTime? CreatedDate { get; set; }
+    public DateTime? ModifiedDate { get; set; }
     public TAcl Acl { get; set; }
     
-    public long CreatedBy { get; set; }
-    public long ModifiedBy { get; set; }
+    public long? CreatedBy { get; set; }
+    public long? ModifiedBy { get; set; }
 }
 
 public interface IAcl
--- NIHR.CRN.CPMS.Common.Tests/Database/RefPerson.cs
+++ NIHR.CRN.CPMS.Common.Tests/Database/RefPerson.cs
@@ -10,13 +10,13 @@ public class RefPerson : IRefPerson<Acl>
     public string? Email { get; set; }
     public string? OrcId { get; set; }
     public bool Active { get; set; }
-    public DateTime CreatedDate { get; set; }
-    public DateTime ModifiedDate { get; set; }
+    public DateTime? CreatedDate { get; set; }
+    public DateTime? ModifiedDate { get; set; }
     public long AclId { get; set; }
 
     public ICollection<UserProfile> UserProfiles { get; } = new List<UserProfile>();
 
     public Acl Acl { get; set; }
-    public long CreatedBy { get; set; }
-    public long ModifiedBy { get; set; }
+    public long? CreatedBy { get; set; }
+    public long? ModifiedBy { get; set; }
 }

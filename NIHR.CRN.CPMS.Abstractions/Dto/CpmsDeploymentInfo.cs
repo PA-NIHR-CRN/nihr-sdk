421c8ec872983e0f55c4d752bd752c6426c4601f
kira.clarke <kira.clarke@nihr.ac.uk>
2026-02-04 08:10:56 +0000

    CRNCC-788

--- NIHR.CRN.CPMS.Abstractions/Dto/CpmsDeploymentInfo.cs
+++ NIHR.CRN.CPMS.Abstractions/Dto/CpmsDeploymentInfo.cs
@@ -2,7 +2,7 @@ namespace NIHR.CRN.CPMS.Abstractions;
 
 public class CpmsDeploymentInfo
 {
-    public EnvironmentInfo EnvironmentInfo { get; set; } = new EnvironmentInfo();
-    public DatabaseInfo DatabaseInfo { get; set; } = new DatabaseInfo();
+    public EnvironmentInfo Environment { get; set; } = new EnvironmentInfo();
+    public DatabaseInfo Database { get; set; } = new DatabaseInfo();
     public ApplicationInfo Application { get; set; } = new ApplicationInfo();
 }

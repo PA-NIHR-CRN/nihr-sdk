2370cae792b5f0ba76a00cc57ede3e5fc02ee0a0
Paul Tennyson <110605969+paul-tennyson@users.noreply.github.com>
2026-08-18 16:10:44 +0100

    CRNCC-3219 Tabs component

---
+++ NIHR.GovUk.AspNetCore.Mvc.Docs/Models/GovUkTabsModel.cs
@@ -0,0 +1,5 @@
+﻿namespace NIHR.GovUk.AspNetCore.Mvc.Docs.Models;
+
+public record GovUkTabsModel(
+    string Title,
+    IEnumerable<GovUkTabModel> Tabs);
---
+++ NIHR.GovUk.AspNetCore.Mvc.Docs/TagHelpers/TabsContext.cs
@@ -0,0 +1,8 @@
+﻿using NIHR.GovUk.AspNetCore.Mvc.Docs.Models;
+
+namespace NIHR.GovUk.AspNetCore.Mvc.Docs.TagHelpers;
+
+public class TabsContext
+{
+    public IList<GovUkTabModel> Tabs { get; } = new List<GovUkTabModel>();
+}

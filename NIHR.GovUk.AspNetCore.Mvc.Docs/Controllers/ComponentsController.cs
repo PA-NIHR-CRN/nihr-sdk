9f0565e264fcad08bb4b5a748074d9be125f5909
kira.clarke <kira.clarke@nihr.ac.uk>
2026-08-18 13:34:53 +0100

    GovUk MVC Docs initial skeleton

---
+++ NIHR.GovUk.AspNetCore.Mvc.Docs/Controllers/ComponentsController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace NIHR.GovUk.AspNetCore.Mvc.Docs.Controllers;
+
+public class ComponentsController : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    public IActionResult Tabs()
+    {
+        return View();
+    }
+}
